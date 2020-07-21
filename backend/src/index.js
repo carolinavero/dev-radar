5c0a47c0a5f039ba9de3fc05223eed8490d68c42
Carolina Vero <carolina@w42.com.br>
2020-07-21 08:09:03 -0300

    update yark lock

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const server = http.Server(app);
 setupWebsocket(server);
 
 //string de conexao - mongodb
-mongoose.connect('mongodb+srv://<user>:<password>@cluster0-xxjkw.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<user>:<password>@cluster0-xxjkw.mongodb.net/week10?retryWrites=true&w=majority', { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
